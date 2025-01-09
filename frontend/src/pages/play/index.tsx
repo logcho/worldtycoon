8616dd0a5ae1b7adee670c1aee7faa0235f0f203
logcho <lsjcwma@gmail.com>
2025-01-08 18:12:53 -0600

    chore: background image

--- frontend/src/pages/play/index.tsx
+++ frontend/src/pages/play/index.tsx
@@ -2,7 +2,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Button from '../components/Button';
+import Button from '../../components/Button';
 import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
@@ -22,17 +22,9 @@ const Home: NextPage = () => {
 
       {/* Content  */}
       {/* h-[calc(100vh-64px)] calculated height from header  */}
-      <div 
-        className='flex flex-row justify-center items-center h-screen w-screen bg-cover bg-center'
-        style={{ backgroundImage: "url('/images/bg.png')" }}
-      >
-
-        <Button 
-          onClick={() => router.push('/play')}
-          className='bg-black h-fit'
-        >
-          <h1 className='text-white font-semibold text-2xl'>Play</h1>
-        </Button>
+      <div className='flex flex-row justify-center items-center h-[calc(100vh-64px)] w-screen'>
+        <h1>play scene</h1>
+        
       </div>
     </>
   );
